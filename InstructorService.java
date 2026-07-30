b17a30980a2dfcbeca236c0666b128d520108a39
hodienhung <lynguyen20013@gmail.com>
2026-07-30 17:53:16 +0700

    Create InstructorService.java

---
+++ InstructorService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Instructor;
+import com.example.demo.repository.InstructorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InstructorService {
+    private final InstructorRepository instructorRepository;
+
+    @Autowired
+    public InstructorService(InstructorRepository instructorRepository) {
+        this.instructorRepository = instructorRepository;
+    }
+
+    public List<Instructor> getAllInstructors() {
+        return instructorRepository.findAll();
+    }
+}
